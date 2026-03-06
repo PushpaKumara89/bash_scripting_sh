1e9f6465b05eeea7a8ea9e25af6c995b055bf528
PushpaKumara89 <kpushpakumarajd@gmail.com>
2026-03-06 12:54:07 +0530

    add commands to traffic-generator.sh file

--- traffic-generator.sh
+++ traffic-generator.sh
@@ -1,1 +1,18 @@
-#!bin/bash
+#!/bin/bash
+if [ "$#" -lt 2 ]; then 
+    echo "Usage: $0 <target> <intervel-in-seconds>"
+    exit 1
+fi
+
+TARGET=$1
+INTERVEL=$2
+
+echo "Sending requests to $TARGET every $INTERVEL seconds..."
+
+while true; do
+    REQUEST_TIME=$(date +"%Y-%m-%d %H:%M:%S")
+    RESPONSE=$(curl -s "$TARGET")
+
+    echo "[$REQUEST_TIME] $RESPONSE"
+    sleep $INTERVEL
+done
